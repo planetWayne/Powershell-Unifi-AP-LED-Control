c23db2366631c87033c4fcc707594e91ea8484ce
planetWayne <2609566+planetWayne@users.noreply.github.com>
2019-03-05 22:09:35 +0000

    Update of readme and powershell documentation
    Basic start of the ReadMe along with getting the Powershell context help to work

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Powershell-Unifi-AP-LED-Control
+
+This little script is for PowerShell to allow you to control the LED status lights on the Ubiquiti Unifi Wireless Access points via either a [Ubiquiti CloudKey](https://www.ui.com/unifi/unifi-cloud-key/) or other [Unifi Controller](https://www.ui.com/software/).
+
+Use is streight forward enough, you can either edit the script to set defaults for things like the BaseURI of the controller, Username, Password and Unifi Site or pass each parameter as a commad line option. If no 'state' is passed then it will return what the current LED state is or specifying 'on' to turn on or 'off' to turn it off again.
+
+Exaples are given in the powershell script, call with 'Unifi-AP-LED-Control.ps1 -?' to get help or 'get-help Unifi-AP-LED-Control.ps1 -examples' to see some examples.
--- Unifi-AP-LEDs.ps1
+++ Unifi-AP-LEDs.ps1
@@ -1,14 +1,32 @@
 ﻿<#
 .SYNOPSIS
-
-Queries state of LED status from a Unifi Controller.
+Gets / Sets the state of a sites LED_STATUS status from a Unifi Controller.
 
 .Description
-
 Simply call script with either 'on' or 'off' to change the state of the Unifi AP ring LED's on the access points.
-If no paramter is given, it returns the current state of the controller.
+
+If no paramter is given, this will return the current state of the sites LED_STATUS.
+
 To use, you can either specify each parameter on the command line or set defaults within the script.
-Change the BaseURI, UnifiUsername, UnifiPassword and UnifiSite's to match your installation.
+Change the BaseURI, Username, Password and Site's to match your installation. In doing this, the only thing that needs to be passed is the 'state' if you want to turn it on or off.
+
+NOTE
+It would appear that if you rename the 'default' site, it will still carry the 'default' name when called via the API.
+
+.example
+.\Unifi-AP-LEDs.ps1   {no parameters}
+
+Will return the currently set state from the default controllers LED_STATE setting, as coded in the script.
+
+.EXAMPLE
+.\Unifi-AP-LEDs.ps1 on
+
+Changes the LED_STATUS on the default unifi controller to ON - thus turning on the AP's status LED
+
+.EXAMPLE
+.\Unifi-AP-LEDs.ps1 on -BaseURI https:\\192.168.1.10:8443 -Username admin -Password mysecret -Site site3
+
+Requests the state of the controller specified with the BaseURI Username and Passwords that are not defaulted in the script.
 
 .PARAMETER State
 
@@ -18,54 +36,40 @@ Change the BaseURI, UnifiUsername, UnifiPassword and UnifiSite's to match your i
 
 Specify the URI for your controller, eg HTTPS://192.168.1.10:8443
 
-.PARAMETER UnifiUsername
+.PARAMETER Username
 
 Manager username for your controller.
 
-.PARAMETER UnifiPassword
+.PARAMETER Password
 
 Password for your specified username.
 
-.PARAMETER UnifiSite
+.PARAMETER Site
 
 Name of the site you want to change.
 Please note that it is possible to rename the default site witout it having to change the name used in the API call.
 
-.NOTE
-
-It would appear that if you rename the 'default' site, it will still carry the 'default' name when called via the API.
+.LINK
+https://www.ui.com/download/unifi/unifi-cloud-key
+.LINK
+https://www.ui.com/unifi/unifi-cloud-key/
+.LINK
+https://www.ui.com/software/
 
 #>
 
 
 
-###############################################################################################################################
-# Simply call script with either 'on' or 'off' to change the state of the Unifi AP ring LED's on the access points.
-# If no paramter is given, the output would reflect the current state that the controller is set to.
-#
-# To use
-# Change the BaseURI, UnifiUsername, UnifiPassword and UnifiSite's to match your installation.
-# Note, it would appear that if you rename the 'default' site, it will still carry the 'default' name when called via the API.
-#
-
-
-
-
-
-
 param([string]$State="",
-    [string]$BaseURI = "HTTPS://192.168.250.11:8443",
-    [string]$UnifiUsername = "ubnt",
-    [string]$UnifiPassword = "midtype1;",
-    [string]$UnifiSite = "default"
+    [string]$BaseURI  = "HTTPS://192.168.1.50:8443",
+    [string]$Username = "ubnt",
+    [string]$Password = "password",
+    [string]$Site     = "default"
 )
 
 
 
-
 # There be demons below...
-
-
 
 # a fudge to get powershell to ignore certificate issues
 add-type @"
@@ -83,14 +87,12 @@ add-type @"
 
 
 $cred = @{}
-$cred.username=$UnifiUsername
-$cred.password=$UnifiPassword
-
+$cred.username=$Username
+$cred.password=$Password
 
 write-host "Logging In"
 
 $result = Invoke-WebRequest -uri "$($BaseURI)/api/login" -SessionVariable 'Session' -Body $($cred|convertto-json) -Method 'POST'
-
 
 $Body=@{}
 
@@ -100,7 +102,8 @@ if ("on","off" -contains $state)
     $Body.led_enabled = $(if($state -eq "on") {"true"} else {"false"})
 }
 
-$result = Invoke-WebRequest -uri "$($BaseURI)/api/s/$($UnifiSite)/set/setting/mgmt"  -WebSession $Session -Body $($Body|convertto-json) -Method 'POST'
+$result = Invoke-WebRequest -uri "$($BaseURI)/api/s/$($Site)/set/setting/mgmt"  -WebSession $Session -Body $($Body|convertto-json) -Method 'POST'
+
 $LED_Status = if( (convertfrom-json $result.content).data.led_enabled -eq "true") {"On"} else {"Off"} 
 
-write-host "System reports status as '$LED_Status'"
+write-host "Controller reports status as '$LED_Status'"
